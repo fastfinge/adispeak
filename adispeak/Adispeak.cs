61e6275c10669ff75eeb5bb0f2d443025f69ec5b
Arthur Pirika <arfy32@gmail.com>
2018-01-29 10:08:25 +0800

    Finished reformatting strings, as they stand. Made nick changes less verbose if they occur in a window which is the current server.

--- adispeak/Adispeak.cs
+++ adispeak/Adispeak.cs
@@ -255,6 +255,7 @@ namespace adispeak
                 }
                 _host.ActiveIWindow.TextView.ScrollTo(CurPos);
                 Tolk.Output(_tools.Strip(_host.ActiveIWindow.TextView.GetLine(CurPos)));
+                _host.ActiveIWindow.Editbox.Text = "";
             }
 
             if (argument.KeyEventArgs.Alt && argument.KeyEventArgs.Shift && argument.KeyEventArgs.KeyCode == Keys.Home)
@@ -935,10 +936,17 @@ namespace adispeak
 
         private void OnNick(NickArgs argument)
         {
-            Tolk.Output($"{argument.User.Nick} now known as {argument.NewNick}");
-        }
+            if (argument.Server.Name == _host.ActiveIWindow.Server.Name)
+            {
+                Tolk.Output($"{argument.User.Nick} now known as {argument.NewNick}");
+            }
+            else
+            {
+                Tolk.Output($"{argument.User.Nick} on {argument.Server.Name} now known as {argument.NewNick}");
+            }
+            }
 
-        private void OnNotifyUserOffline(NotifyUserOfflineArgs argument)
+            private void OnNotifyUserOffline(NotifyUserOfflineArgs argument)
         {
             if (argument.Server.Name == _host.ActiveIWindow.Name)
             {
@@ -964,17 +972,17 @@ namespace adispeak
 
         private void OnPrivateActionMessage(PrivateActionMessageArgs argument)
         {
-            Tolk.Output($"{argument.User.Nick}@{argument.Server.Name} in private message {_tools.Strip(argument.Message)}");
+                Tolk.Output($"{argument.User.Nick}@{argument.Server.Name} in private message {_tools.Strip(argument.Message)}");
         }
 
         private void OnPrivateCtcpMessage(PrivateCtcpMessageArgs argument)
         {
-            Tolk.Output($"{argument.User.Nick}@{argument.Server.Name} in ctcp message says {_tools.Strip(argument.Message)}");
+                Tolk.Output($"{argument.User.Nick}@{argument.Server.Name} in ctcp message says {_tools.Strip(argument.Message)}");
         }
 
         private void OnPrivateCtcpReplyMessage(PrivateCtcpReplyMessageArgs argument)
         {
-            Tolk.Output($"{argument.User.Nick}@{argument.Server.Name} in ctcp reply says {_tools.Strip(argument.Message)}");
+                Tolk.Output($"{argument.User.Nick}@{argument.Server.Name} in ctcp reply says {_tools.Strip(argument.Message)}");
         }
 
         private void OnPrivateNormalMessage(PrivateNormalMessageArgs argument)
@@ -989,13 +997,13 @@ namespace adispeak
 
         private void OnQuit(QuitArgs argument)
         {
-            if (argument.Server.Network == _host.ActiveIWindow.Server.Network)
+            if (argument.Server.Name == _host.ActiveIWindow.Server.Name)
             {
                 Tolk.Output($"{argument.User.Nick} quit: {_tools.Strip(argument.QuitMessage)}");
             }
             else
             {
-                Tolk.Output($"{argument.User.Nick} on {argument.Server.Network} quit: {_tools.Strip(argument.QuitMessage)}");
+                Tolk.Output($"{argument.User.Nick} on {argument.Server.Name} quit: {_tools.Strip(argument.QuitMessage)}");
             }
         }
 
@@ -1057,18 +1065,18 @@ namespace adispeak
         {
             if (SayTopic && argument.Numeric == "332")
             {
-                string[] param = argument.Message.Split(' ');
-                string topic = string.Join(" ", param, 2, param.Length - 2);
-                Tolk.Output("The topic is: " + topic + ".");
+                string[] Param = argument.Message.Split(' ');
+                string Topic = string.Join(" ", Param, 2, Param.Length - 2);
+                Tolk.Output($"The topic is: {_tools.Strip(Topic)}.");
                 SayTopic = false;
             }
             if (SayTopicSetBy && argument.Numeric == "333")
             {
-                string[] param = argument.Message.Split(' ');
-                string topicSetBy = param[2];
-                Int64 timeStamp = Convert.ToInt64(param[3]);
-                string topicTime = DateTimeOffset.FromUnixTimeSeconds(timeStamp).ToLocalTime().ToString("f");
-                Tolk.Output("Topic set by " + topicSetBy + " on " + topicTime + ".");
+                string[] Param = argument.Message.Split(' ');
+                string TopicSetBy = Param[2];
+                Int64 TimeStamp = Convert.ToInt64(Param[3]);
+                string TopicTime = DateTimeOffset.FromUnixTimeSeconds(TimeStamp).ToLocalTime().ToString("f");
+                Tolk.Output($"Topic set by {TopicSetBy} on {TopicTime}.");
                 SayTopicSetBy = false;
             }
         }
