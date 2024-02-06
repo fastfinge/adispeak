c7b8edd89008d1ae82e157a4f53b89b9250b5002
Samuel Proulx <samuel@interfree.ca>
2024-02-06 00:44:30 -0500

    Wait why didn't it add the new files

---
+++ adispeak/README.md
@@ -0,0 +1,15 @@
+# adispeak
+Solution to make AdiIRC interface with screen readers.
+
+## Latest Preconfigured Adispeak
+
+Get adispeak alpha 0.2.3 from:
+https://github.com/fastfinge/adispeak/releases/download/v_0.2.3/adispeak_0.2.3.zip
+
+## Installing
+
+Put everything in adispeak\bin\x64\release into the same folder as AdiIRC.exe. Run AdiIRC.exe, go to file, plugins, then load. Load adispeak.dll.  It should work!  
+
+## Building
+
+Have Visual Studio 2017. Do the obvious things; the project should come with everything you need!
---
+++ adispeak/gui.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace adispeak
+{
+
+    public class ConfigGui: Form
+    {
+        private bool locked;
+        private TreeView treeView;
+        private Button okButton;
+        private Config conf;
+        private Dictionary<string, bool> items;
+        private Dictionary <string, bool> AvailableOptions = new Dictionary <string, bool>
+            {
+            {"OnChannelActionMessage", true},
+            {"OnChannelCtcpMessage", true},
+            {"OnChannelCtcpReplyMessage", true},
+            {"OnChannelInvite", true},
+            {"OnChannelJoin", true},
+            {"OnChannelKick", true},
+            {"OnChannelModeListBan", true},
+            {"OnChannelModeListBanExempt", true},
+            {"OnChannelModeListBanUnexempt", true},
+            {"OnChannelModeListInviteExempt", true},
+            {"OnChannelModeListInviteUnexempt", true},
+            {"OnChannelModeListQuiet", true},
+            {"OnChannelModeListUnban", true},
+            {"OnChannelModeListUnquiet", true},
+            {"OnChannelModeUserAdmined", true},
+            {"OnChannelModeUserDeadmined", true},
+            {"OnChannelModeUserDehalfOpped", true},
+            {"OnChannelModeUserDeopped", true},
+            {"OnChannelModeUserDeownered", true},
+            {"OnChannelModeUserDevoiced", true},
+            {"OnChannelModeUserHalfOpped", true},
+            {"OnChannelModeUserOpped", true},
+            {"OnChannelModeUserOwnered", true},
+            {"OnChannelModeUserVoiced", true},
+            {"OnChannelNormalMessage", true},
+            {"OnChannelNoticeMessage", true},
+            {"OnChannelPart", true},
+            {"OnChannelServerModeListBan", true},
+            {"OnChannelServerModeListBanExempt", true},
+            {"OnChannelServerModeListBanUnexempt", true},
+            {"OnChannelServerModeListInviteExempt", true},
+            {"OnChannelServerModeListInviteUnexempt", true},
+            {"OnChannelServerModeListQuiet", true},
+            {"OnChannelServerModeListUnban", true},
+            {"OnChannelServerModeListUnquiet", true},
+            {"OnChannelServerModeUserAdmined", true},
+            {"OnChannelServerModeUserDeadmined", true},
+            {"OnChannelServerModeUserDehalfOpped", true},
+            {"OnChannelServerModeUserDeopped", true},
+            {"OnChannelServerModeUserDeownered", true},
+            {"OnChannelServerModeUserDevoiced", true},
+            {"OnChannelServerModeUserHalfOpped", true},
+            {"OnChannelServerModeUserOpped", true},
+            {"OnChannelServerModeUserOwnered", true},
+            {"OnChannelServerModeUserVoiced", true},
+            {"OnChannelTopic", true},
+            {"OnConnect", true},
+            {"OnConnectFailure", true},
+            {"OnConnectionLogonSuccess", true},
+            {"OnDisconnect", true},
+            {"OnMessageSent", true},
+            {"OnNick", true},
+            {"OnNotifyUserOffline", true},
+            {"OnNotifyUserOnline", true},
+            {"OnPrivateActionMessage", true},
+            {"OnPrivateCtcpMessage", true},
+            {"OnPrivateNormalMessage", true},
+            {"OnPrivateNoticeMessage", true},
+            {"OnQuit", true},
+            {"OnServerErrorMessage", true},
+            {"OnServerNoticeMessage", true},
+            {"OnUserInvitedToChannel", true},
+            {"OnUserMode", true}
+            };
+
+        public ConfigGui(Config confparm)
+        {
+            conf = confparm;
+            treeView = new TreeView();
+            treeView.CheckBoxes = true;
+            treeView.AfterCheck += new TreeViewEventHandler(treeView_AfterCheck);
+            items = conf.GetWindowList();
+            locked = true;
+            foreach (var item in items)
+            {
+                treeView.Nodes.Add(item.Key, item.Key);
+                foreach (var option in AvailableOptions)
+                {
+                    if (option.Key != "speech")
+                    {
+                        treeView.Nodes[item.Key].Nodes.Add(option.Key, option.Key);
+                        treeView.Nodes[item.Key].Nodes[option.Key].Checked = conf.GetWindow(item.Key, option.Key);
+                    }
+                    else
+                    {
+                        treeView.Nodes[item.Key].Checked = conf.GetWindow(item.Key, option.Key);
+                    }
+                }
+            }
+            treeView.Dock = DockStyle.Fill;
+            Controls.Add(treeView);
+            locked = false;
+            okButton = new Button();
+            okButton.Text = "OK";
+            okButton.Dock = DockStyle.Bottom;
+            okButton.Click += new EventHandler(okButton_Click);
+            Controls.Add(okButton);
+
+            this.Size = new System.Drawing.Size(300, 300);
+            this.Show();
+        }
+
+        private void treeView_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            if (locked == false)
+            {
+                ToggleNode(e.Node);
+            }
+        }
+
+        private void ToggleNode(TreeNode node)
+        {
+            if (node.Parent != null)
+            {
+                locked = true;
+                foreach (TreeNode tempnode in node.Parent.Nodes)
+                {
+                    if (tempnode.Checked == true)
+                    {
+                        node.Parent.Checked = true;
+                    }
+                }
+                conf.SetWindow(node.Parent.Name, node.Name, node.Checked);
+                locked = false;
+            }
+            else
+            {
+                locked = true;
+                conf.SetWindow(node.Name, "speech", node.Checked);
+                foreach (TreeNode tempnode in node.Nodes)
+                {
+                    tempnode.Checked = node.Checked;
+                }
+                locked = false;
+            }
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            conf.Write("speech.json");
+           this.Hide();
+        }
+    }
+}
